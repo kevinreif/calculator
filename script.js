8eb183c0fc5772e21b1249a02d122eb972add12a
Kevin Reif <kevin.reif@outlook.com>
2022-04-04 18:28:03 -0700

    add basic maths functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subrtract(a, b) {
+    return a - b;
+}
+
+function mulitply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
